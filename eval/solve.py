037f5ef4a0d8f6b19d93de567167a528df9c9814
Max Proskauer <mprosk@gmail.com>
2021-12-20 21:23:59 -0500

    Add new equation from new data

--- eval/solve.py
+++ eval/solve.py
@@ -1,95 +1,46 @@
+import math
 
-# Full decimal accuracy
-
-def freq_full(x, y):
-    return -1917.6297513694317 + 16.98125248102388 * x + 0.08482089338894398 * y + 0.026416174905564915 * x * y + 0.007379870737151961 * y ** 2
-
-def freq_full_5k(x, y):
-    return -1503.0871470775444 + (12.076708904712229 * x) + (0.9015390804265535 + 0.011523262414507407 * x + 0.0032597268956831016 * y) * y
-
-
-# 7 digit floats
 
 def freq(x, y):
-    return -1917.6297514 + (16.9812525 * x) + (0.0848209 + 0.0264162 * x + 0.0073799 * y) * y
+    """5k coarse, 5k fine parallel with 1.5k"""
+    return 0.6351428 + (0.0048308 * x) + (-0.0001811 + 9e-07 * x + 1.2e-06 * y) * y
 
 
-def freq_5k(x, y):
-    return -1503.0871471 + (12.0767089 * x) + (0.9015391 + 0.0115233 * x + 0.0032597 * y) * y
+def coarse(z):
+    """Coarse pot setting that gets just above the target freq with fine=255"""
+    return min(max(math.ceil(197.617 * z - 131.809), 0), 255)
 
-# Fine fixed at 127
-
-def freq_127(x):
-    return 20.3361 * x - 1787.83
-
-def freq_127_5k(x):
-    return 13.4281111 * x - 1272.9
-
-
-def dump():
-    with open('dump.csv', mode='w') as fp:
-        for x in range(256):
-            for y in range(256):
-                fp.write('{},{},{}\n'.format(x, y, round(f(x, y), 0)))
-
-
-def dump_2d():
-    with open('dump_2d.csv', mode='w') as fp:
-        header = [str(n) for n in range(256)]
-        fp.write(',' + ','.join(header) + '\n')
-        for x in range(256):
-            fp.write('{}'.format(x))
-            for y in range(256):
-                fp.write(',{}'.format(round(f(x, y), 3)))
-            fp.write('\n')
-
-
-def is_close(val, target, tol=0.2):
-    lo = (1 - tol) * target
-    hi = (1 + tol) * target
-    return (lo <= val) and (val <= hi)
+################################################################################
 
 
 def solve(z):
-    x_min = 0
-    x_max = 255
-    y_min = 0
-    y_max = 255
 
-    y = (y_max + y_min) // 2
+    # 1. Calculate the coarse value (fine=255) that puts us just above the target
+    x = coarse(z)
+    print("x =", x)
+    print('f({}) ='.format(x), freq(x, 255))
+    print('f({}) ='.format(x - 1), freq(x - 1, 255))
 
-    n = 0
-
-    # get x close
-    for _ in range(8):
-        x = (x_max + x_min) // 2
-        f = freq_127(x)
-        print('Step {}, x = {} ({}, {}), f = {}'.format(n, x, x_min, x_max, f))
-        n += 1
-        if z < f:
-            x_max = x
-        elif z > f:
-            x_min = x
-    print('X =', x)
-
-    # get y closer
-    for _ in range(8):
-        y = (y_max + y_min) // 2
+    # 2. Binary search for the y value
+    ymin = 0
+    ymax = 255
+    y = 127
+    for i in range(8):
+        y = (ymax + ymin) // 2
         f = freq(x, y)
-        print('Step {}, y = {} ({}, {}), f = {}'.format(n, y, y_min, y_max, f))
-        n += 1
+        print('Step {}, y = {} ({}, {}), f = {}'.format(i, y, ymin, ymax, f))
         if z < f:
-            y_max = y
+            ymax = y
         elif z > f:
-            y_min = y
-
-    print('Y =', y)
-
+            ymin = y
+        else:
+            break
+    print('y =', y)
     return x, y
 
 
 if __name__ == '__main__':
-    for z in [1950, -440, 3000, -840, -1000, 420, 0]:
+    for z in [1, 0.75, 1.115, 1.8, 0.5, 2]:
         print()
         print('-' * 30)
         print("Solving for Z =", z)
@@ -98,6 +49,6 @@ if __name__ == '__main__':
         err = 'N/A'
         try:
             err = abs((f - z) / z)
-        except:
+        except ZeroDivisionError:
             pass
         print('X = {}, Y = {}, Z = {}, f = {}, error = {}'.format(x, y, z, f, err))
